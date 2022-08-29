c77da99cd5469319588897572bb215e828d5cf6a
Elizabeth Wandia Mugo <wwandia173@gmail.com>
2022-08-29 22:48:26 +0300

    Done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,11 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+end
+
+class AddFavoriteFlowerToArtists < ActiveRecord::Migration[6.1]
+    def change
+      add_column :artists, :favorite_flower, :string
+    end
+  end
+
+  
---
+++ db/migrate/20220829193258_add_favorite_flower_to_artists.rb
@@ -0,0 +1,4 @@
+class AddFavoriteFlowerToArtists < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
